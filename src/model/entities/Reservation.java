ff6732b8ececbd75d63abd9c3a0350e7874eff5d
caick Tanan <caick_tanan@hotmail.com>
2020-05-14 14:54:30 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -6,16 +6,11 @@ import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
-	
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Reservation() {
-		
-	}
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
@@ -31,44 +26,44 @@ public class Reservation {
 		this.roomNumber = roomNumber;
 	}
 
-	public Date getcheckIn() {
+	public Date getCheckIn() {
 		return checkIn;
 	}
 
-	public void setcheckIn(Date checkIn) {
-		this.checkIn = checkIn;
-	}
-
-	public Date getcheckOut() {
+	public Date getCheckOut() {
 		return checkOut;
 	}
 
-	public void setcheckOut(Date checkOut) {
-		this.checkOut = checkOut;
-	}
-	
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
+			
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Room "
-				+roomNumber
-				+", check-in: "
-				+sdf.format(checkIn)
-				+", check-out: "
-				+sdf.format(checkOut)
-				+", "
-				+ duration()
-				+" nights";
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
 	}
-	
-	
 }
